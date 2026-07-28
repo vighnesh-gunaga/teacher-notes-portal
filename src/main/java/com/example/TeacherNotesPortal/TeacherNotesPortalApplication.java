030fe7a70d5c6489d9bba8e5e91006ec907831ca
vighnesh-gunaga <vighneshsg07@gmail.com>
2026-07-28 14:17:21 +0530

    Initial Spring Boot Project

---
+++ src/main/java/com/example/TeacherNotesPortal/TeacherNotesPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeacherNotesPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherNotesPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherNotesPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TeacherNotesPortal/TeacherNotesPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeacherNotesPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherNotesPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
